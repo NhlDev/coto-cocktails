519de91839b30356f6d1021e08487111b3a39ee1
Nahuel Alderete <nahuel.ald@gmail.com>
2025-10-17 15:38:33 -0300

    feat: clase base de servicio y configuraciones necesarias para la configuracion de la api url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BASE_API_URL } from './core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // uso este token para definir la URL de la API. Lo dejo harcodeado por simplicidad, pero en una app real podría venir de una variable de entorno, un archivo environment o similar
+    { provide: BASE_API_URL, useValue: 'https://www.thecocktaildb.com/api/json/v1/1/' },
   ]
 };
---
+++ src/app/core/services/base-api.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { BASE_API_URL } from "..";
+
+export abstract class BaseApi {
+    private http = inject(HttpClient);
+    private readonly API_URL = inject(BASE_API_URL);
+
+
+    // Se implementa solo el GET por una cuestion de que los demas verbos HTTP no son necesarios en este proyecto
+    protected get<T>(url: string, params?: any) {
+        return this.http.get<T>(`${this.API_URL}${url}`, { params });
+    }
+}
